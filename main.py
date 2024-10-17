9c9c72750ffd2c576288286e93c56e6d27a4be0d
j-rein26 <grow435@gmail.com>
2024-10-17 11:11:48 -0400

    added send_email code and also the body for the email.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+from send_email import send_email
 
 api_key = "f7b82d9c60fe4de39a1276f6da8356da"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
     "from=2024-09-17&sortBy=publishedAt&apiKey=" \
         "f7b82d9c60fe4de39a1276f6da8356da"
+
 
 # make a request
 request = requests.get(url)
@@ -12,8 +14,14 @@ request = requests.get(url)
 content = request.json()
 
 # Access the articles with descriptions
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
 
     
+    
+
