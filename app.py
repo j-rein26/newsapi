ad442163474e1ca1b5855aa9476e50136c758a19
j-rein26 <grow435@gmail.com>
2024-10-17 17:15:44 -0400

    Changed app.py to send top 20 headlines in the US.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from send_email import send_email
 
 
 api_key = "f7b82d9c60fe4de39a1276f6da8356da"
-url = "https://newsapi.org/v2/top-headlines?country=us" \
+url = "https://newsapi.org/v2/top-headlines/?country=us" \
 "&apiKey=f7b82d9c60fe4de39a1276f6da8356da"
 
 
@@ -11,11 +11,11 @@ request = requests.get(url)
 content = request.json()
 
 
-body = "Subject: Today's News From Turkey" + "\n"
+body = "Subject: Today's Headlines" + "\n"
 for article in content["articles"][0:20]:
-    if article["title"] is not None and article["description"] is not None:
-        body = body + article["title"] + "\n" \
-        + article["description"] + "\n" \
+    
+    body = body + article["author"] + "\n" \
+        + article["title"] + "\n" \
         + article["url"]
 
 body = body.encode("utf-8")
